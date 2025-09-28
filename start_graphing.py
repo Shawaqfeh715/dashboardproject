9e7cd041bb712f06481810c31363fe13ee432035
Shawaqfeh715 <Anasshaw@umich.edu>
2025-09-27 20:31:54 -0400

    2

--- start_graphing.py
+++ start_graphing.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
-import csv
+from matplotlib.animation import FuncAnimation
 
-hadestest=''
+hadestest='/Users/anasshaw/Documents/GitHub/dashboardproject/HADESTestData.csv'
+df=pd.read_csv(hadestest)
 
-
-
-
+print(df)
 
